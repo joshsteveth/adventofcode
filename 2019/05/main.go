ae7fbd66a10d10a44ddae3a7332045682e8aeb1d
Josh <joshsteveth@gmail.com>
2019-12-11 17:53:30 +0100

    part two

--- 2019/05/main.go
+++ 2019/05/main.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/joshsteveth/adventofcode/util"
 )
 
 const inp = `3,225,1,225,6,6,1100,1,238,225,104,0,1102,72,20,224,1001,224,-1440,224,4,224,102,8,223,223,1001,224,5,224,1,224,223,223,1002,147,33,224,101,-3036,224,224,4,224,102,8,223,223,1001,224,5,224,1,224,223,223,1102,32,90,225,101,65,87,224,101,-85,224,224,4,224,1002,223,8,223,101,4,224,224,1,223,224,223,1102,33,92,225,1102,20,52,225,1101,76,89,225,1,117,122,224,101,-78,224,224,4,224,102,8,223,223,101,1,224,224,1,223,224,223,1102,54,22,225,1102,5,24,225,102,50,84,224,101,-4600,224,224,4,224,1002,223,8,223,101,3,224,224,1,223,224,223,1102,92,64,225,1101,42,83,224,101,-125,224,224,4,224,102,8,223,223,101,5,224,224,1,224,223,223,2,58,195,224,1001,224,-6840,224,4,224,102,8,223,223,101,1,224,224,1,223,224,223,1101,76,48,225,1001,92,65,224,1001,224,-154,224,4,224,1002,223,8,223,101,5,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,1107,677,226,224,1002,223,2,223,1005,224,329,101,1,223,223,7,677,226,224,102,2,223,223,1005,224,344,1001,223,1,223,1107,226,226,224,1002,223,2,223,1006,224,359,1001,223,1,223,8,226,226,224,1002,223,2,223,1006,224,374,101,1,223,223,108,226,226,224,102,2,223,223,1005,224,389,1001,223,1,223,1008,226,226,224,1002,223,2,223,1005,224,404,101,1,223,223,1107,226,677,224,1002,223,2,223,1006,224,419,101,1,223,223,1008,226,677,224,1002,223,2,223,1006,224,434,101,1,223,223,108,677,677,224,1002,223,2,223,1006,224,449,101,1,223,223,1108,677,226,224,102,2,223,223,1006,224,464,1001,223,1,223,107,677,677,224,102,2,223,223,1005,224,479,101,1,223,223,7,226,677,224,1002,223,2,223,1006,224,494,1001,223,1,223,7,677,677,224,102,2,223,223,1006,224,509,101,1,223,223,107,226,677,224,1002,223,2,223,1006,224,524,1001,223,1,223,1007,226,226,224,102,2,223,223,1006,224,539,1001,223,1,223,108,677,226,224,102,2,223,223,1005,224,554,101,1,223,223,1007,677,677,224,102,2,223,223,1006,224,569,101,1,223,223,8,677,226,224,102,2,223,223,1006,224,584,1001,223,1,223,1008,677,677,224,1002,223,2,223,1006,224,599,1001,223,1,223,1007,677,226,224,1002,223,2,223,1005,224,614,101,1,223,223,1108,226,677,224,1002,223,2,223,1005,224,629,101,1,223,223,1108,677,677,224,1002,223,2,223,1005,224,644,1001,223,1,223,8,226,677,224,1002,223,2,223,1006,224,659,101,1,223,223,107,226,226,224,102,2,223,223,1005,224,674,101,1,223,223,4,223,99,226`
-
-const input = 1
+const input = 5
 
 var output int
 
@@ -32,13 +32,17 @@ const (
 	ocTwo   opcode = 2
 	ocThree opcode = 3
 	ocFour  opcode = 4
+	ocFive  opcode = 5
+	ocSix   opcode = 6
+	ocSeven opcode = 7
+	ocEight opcode = 8
 	oc99    opcode = 99
 )
 
-type opcodeFunc func([]int, ...int) (bool, error)
+type opcodeFunc func([]int, *int, ...int) (bool, error)
 
 var opcodes = map[opcode]opcodeFunc{
-	ocOne: func(arr []int, args ...int) (bool, error) {
+	ocOne: func(arr []int, _ *int, args ...int) (bool, error) {
 		if len(args) != 3 {
 			return false, errInvalidArgsNum
 		}
@@ -46,7 +50,7 @@ var opcodes = map[opcode]opcodeFunc{
 		arr[args[2]] = arr[args[0]] + arr[args[1]]
 		return false, nil
 	},
-	ocTwo: func(arr []int, args ...int) (bool, error) {
+	ocTwo: func(arr []int, _ *int, args ...int) (bool, error) {
 		if len(args) != 3 {
 			return false, errInvalidArgsNum
 		}
@@ -54,7 +58,7 @@ var opcodes = map[opcode]opcodeFunc{
 		arr[args[2]] = arr[args[0]] * arr[args[1]]
 		return false, nil
 	},
-	ocThree: func(arr []int, args ...int) (bool, error) {
+	ocThree: func(arr []int, _ *int, args ...int) (bool, error) {
 		if len(args) != 1 {
 			return false, errInvalidArgsNum
 		}
@@ -62,15 +66,65 @@ var opcodes = map[opcode]opcodeFunc{
 		arr[args[0]] = input
 		return false, nil
 	},
-	ocFour: func(arr []int, args ...int) (bool, error) {
+	ocFour: func(arr []int, _ *int, args ...int) (bool, error) {
 		if len(args) != 1 {
 			return false, errInvalidArgsNum
 		}
 		output = arr[args[0]]
 		return false, nil
 	},
-	oc99: func(arr []int, args ...int) (bool, error) {
+	ocFive: func(arr []int, idx *int, args ...int) (bool, error) {
+		if len(args) != 2 {
+			return false, errInvalidArgsNum
+		}
+
+		if arr[args[0]] == 0 {
+			return false, nil
+		}
+
+		*idx = arr[args[1]]
 		return true, nil
+	},
+	ocSix: func(arr []int, idx *int, args ...int) (bool, error) {
+		if len(args) != 2 {
+			return false, errInvalidArgsNum
+		}
+
+		if arr[args[0]] != 0 {
+			return false, nil
+		}
+
+		*idx = arr[args[1]]
+		return true, nil
+	},
+	ocSeven: func(arr []int, _ *int, args ...int) (bool, error) {
+		if len(args) != 3 {
+			return false, errInvalidArgsNum
+		}
+
+		var val int
+		if arr[args[0]] < arr[args[1]] {
+			val = 1
+		}
+
+		arr[args[2]] = val
+		return false, nil
+	},
+	ocEight: func(arr []int, _ *int, args ...int) (bool, error) {
+		if len(args) != 3 {
+			return false, errInvalidArgsNum
+		}
+
+		var val int
+		if arr[args[0]] == arr[args[1]] {
+			val = 1
+		}
+
+		arr[args[2]] = val
+		return false, nil
+	},
+	oc99: func(arr []int, _ *int, args ...int) (bool, error) {
+		return false, nil
 	},
 }
 
@@ -80,10 +134,14 @@ func (oc opcode) numArgs() int {
 		ocTwo:   3,
 		ocThree: 1,
 		ocFour:  1,
+		ocFive:  2,
+		ocSix:   2,
+		ocSeven: 3,
+		ocEight: 3,
 	}[oc]
 }
 
-func modeOne(arr []int, ocInput int, args ...int) (bool, error) {
+func modeOne(arr []int, ocInput int, idx *int, args ...int) (bool, error) {
 
 	ocstring := fmt.Sprintf("%05d", ocInput)
 	ocint, err := strconv.Atoi(ocstring[3:5])
@@ -119,6 +177,8 @@ func modeOne(arr []int, ocInput int, args ...int) (bool, error) {
 		args[i] = arr[args[i]]
 	}
 
+	var increment bool
+
 	switch oc {
 	case ocOne:
 		arr[args[2]] = args[0] + args[1]
@@ -128,11 +188,35 @@ func modeOne(arr []int, ocInput int, args ...int) (bool, error) {
 		arr[args[0]] = input
 	case ocFour:
 		output = args[0]
+	case ocFive:
+		if args[0] == 0 {
+			break
+		}
+		*idx = args[1]
+		increment = true
+	case ocSix:
+		if args[0] != 0 {
+			break
+		}
+		*idx = args[1]
+		increment = true
+	case ocSeven:
+		var val int
+		if args[0] < args[1] {
+			val = 1
+		}
+		arr[args[2]] = val
+	case ocEight:
+		var val int
+		if args[0] == args[1] {
+			val = 1
+		}
+		arr[args[2]] = val
 	default:
 		return false, fmt.Errorf("invalid situation %v %v", oc, args)
 	}
 
-	return false, nil
+	return increment, nil
 
 }
 
@@ -148,6 +232,11 @@ func getOC(ocInput int) (opcode, error) {
 
 func main() {
 
+	t := time.Now()
+	defer func(t time.Time) {
+		fmt.Printf("runtime: %v\n", time.Since(t))
+	}(t)
+
 	arrString := strings.Split(inp, ",")
 	arr := make([]int, len(arrString))
 	for i := range arrString {
@@ -158,6 +247,7 @@ func main() {
 
 	var i int
 	for {
+
 		oc := opcode(arr[i])
 		if oc == oc99 {
 			break
@@ -177,18 +267,18 @@ func main() {
 
 		ocf, ok := opcodes[oc]
 		if !ok {
-			ocf = func(arr []int, args ...int) (bool, error) {
-				return modeOne(arr, int(oc), args...)
+			ocf = func(arr []int, idx *int, args ...int) (bool, error) {
+				return modeOne(arr, int(oc), idx, args...)
 			}
 		}
 
-		finished, err := ocf(arr, args...)
+		increment, err := ocf(arr, &i, args...)
 		if err != nil {
 			panic(err)
 		}
 
-		if finished {
-			break
+		if increment {
+			continue
 		}
 
 		i++
